b232152d3401f985bf0afd38b19255586c22b4bc
Phuc-he <josphuc2k3@gmail.com>
2026-04-20 10:47:28 +0700

    update

--- src/main/java/com/javaweb/config/ModelMapperConfig.java
+++ src/main/java/com/javaweb/config/ModelMapperConfig.java
@@ -2,11 +2,9 @@ package com.javaweb.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.javaweb")
 public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper(){
